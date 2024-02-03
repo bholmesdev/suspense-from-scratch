1e08c3aa099f6af3eaed41bc5f2ebce73b1ef189
bholmesdev <bholmesdev@gmail.com>
2024-02-03 15:09:55 -0500

    chore: config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
