a28ddce3765a1277791187fc0731f9ee4edf6d6b
bholmesdev <bholmesdev@gmail.com>
2024-02-06 09:10:02 -0500

    deps: react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: node({
     mode: "standalone"
